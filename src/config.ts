8085f32c2401008fe33b2fcbcbbaccb1a2e1afba
Dom-iha <taqib.ibrahim@gmail.com>
2024-08-29 20:27:57 +0100

    deployment fix

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,35 @@
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
 // const dotenv = require("dotenv")
 
-dotenv.config()
+dotenv.config();
 
-const { APPLICATION_ID, PUBLIC_KEY, DISCORD_TOKEN, GUILD_ID, NOTIFICATION_CHANNEL_ID, OPENAI_API_KEY, GOOGLE_API_KEY } = process.env
+const {
+  APPLICATION_ID,
+  PUBLIC_KEY,
+  DISCORD_TOKEN,
+  GUILD_ID,
+  NOTIFICATION_CHANNEL_ID,
+  GOOGLE_API_KEY,
+} = process.env;
 
-if (!APPLICATION_ID || !PUBLIC_KEY || !DISCORD_TOKEN || !GUILD_ID || !NOTIFICATION_CHANNEL_ID || !OPENAI_API_KEY || !GOOGLE_API_KEY) {
-  throw new Error("Missing environment variables")
+if (
+  !APPLICATION_ID ||
+  !PUBLIC_KEY ||
+  !DISCORD_TOKEN ||
+  !GUILD_ID ||
+  !NOTIFICATION_CHANNEL_ID ||
+  !GOOGLE_API_KEY
+) {
+  throw new Error('Missing environment variables');
 }
 
 const config: Record<string, string> = {
-   APPLICATION_ID,
-   PUBLIC_KEY,
-   DISCORD_TOKEN,
-   GUILD_ID,
-   NOTIFICATION_CHANNEL_ID,
-   OPENAI_API_KEY,
-   GOOGLE_API_KEY
-}
+  APPLICATION_ID,
+  PUBLIC_KEY,
+  DISCORD_TOKEN,
+  GUILD_ID,
+  NOTIFICATION_CHANNEL_ID,
+  GOOGLE_API_KEY,
+};
 
-export default config
+export default config;
